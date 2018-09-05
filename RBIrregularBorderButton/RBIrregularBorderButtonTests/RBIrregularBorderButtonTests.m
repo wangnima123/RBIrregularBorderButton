7920bde1cb24e997a12d34d5d18614d008528a46
王泽 <wangze@qudian.com>
2018-09-05 11:52:25 +0800

    新建iOS工程文件

---
+++ RBIrregularBorderButton/RBIrregularBorderButtonTests/RBIrregularBorderButtonTests.m
@@ -0,0 +1,39 @@
+//
+//  RBIrregularBorderButtonTests.m
+//  RBIrregularBorderButtonTests
+//
+//  Created by QuDian on 2018/9/5.
+//  Copyright © 2018年 QuDian. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface RBIrregularBorderButtonTests : XCTestCase
+
+@end
+
+@implementation RBIrregularBorderButtonTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
